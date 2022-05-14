c898b85681046a45dc8633d39a6007214e0a2425
Ramona Snider <41329897+rlsnider@users.noreply.github.com>
2022-05-13 22:45:44 -0700

    Added directional gifs and updated scope

--- index.js
+++ index.js
@@ -2,55 +2,33 @@ const inventory = newInventory()
 move(inventory).to(0, 0)
 
 const character = newImage('assets/green-character/static.gif')
+
+function handleDirectionChange(direction){   //wrap direction instructions for movement gifs in function for scope
+    if(direction === null){
+        character.src = 'assets/green-character/static.gif'
+    }
+    if(direction === 'west'){
+        character.src = 'assets/green-character/west.gif'
+    }
+    if(direction === 'east'){
+        character.src = 'assets/green-character/east.gif'
+    }
+    if(direction === 'north'){
+        character.src = 'assets/green-character/north.gif'
+    }
+    if(direction === 'south'){
+        character.src = 'assets/green-character/south.gif'
+    }
+}
+
+move(character).withArrowKeys(100,250, handleDirectionChange)
 let direction = null; //so we can track character with a variable 
 let x = 100;
 let y = 250;
-
-function moveCharacter(){ //wrap directions in function
-    if(direction === 'west') {    //defined what "direction" and name of direction would document
-        x = x - 1
-    }
-    if(direction === 'east') {
-        x = x + 1
-    }
-    if(direction === 'south') {
-        y = y - 1
-    }
-    if(direction === 'north') {
-        y = y + 1
-    }
-
-character.style.left = x + 'px'//???
-character.style.bottom = y + 'px'
-}
-
-setInterval(moveCharacter, 1)                  //passes the funtion to setInterval and moves greenie every 1 millisecond
-document.addEventListener('keydown', function (e){   //change greenie's direction with arrow keys
-
-    if(e.repeat) return;                  //Where do "e.repeat, e.key,"come from? Not defined in program
-
-    if(e.key === "ArrowLeft"){
-        direction ='west'
-    }
-    if(e.key === 'ArrowUp'){
-        direction = 'north'
-    }
-    if (e.key === "ArrowRight"){
-        direction = 'east'
-    }
-    if (e.key === 'ArrowDown'){
-        direction = 'south'
-    }
-})
-
-document.addEventListener('keyup', function (e){    //greenie stops when key is lifted up
-    direction = null
-})
-
 move(character).to(100, 250)
 
 
-move(newImage('assets/tree.png')).to(200, 450)
+move(newImage ('assets/tree.png')).to(250, 400)
 move(newImage('assets/pillar.png')).to(350, 250)
 move(newImage('assets/pine-tree.png')).to(450, 350)
 move(newImage('assets/crate.png')).to(150, 350)
--- move.js
+++ move.js
@@ -5,9 +5,62 @@ function move(element) {
         element.style.left = left + 'px'
         element.style.bottom = bottom + 'px'
     }
+    function moveWithArrowKeys(left, bottom, callback){  //define function inside of move and attach it to the object we return
+            let direction = null;
+            let x = left;
+            let y = bottom;
+
+            element.style.left = x + 'px'
+            element.style.bottom = y + 'px'
+
+            function moveCharacter(){ //wrap directions in function
+                if(direction === 'west') {    //defined what "direction" and name of direction would document
+                    x = x - 1
+                }
+                if(direction === 'east') {
+                    x = x + 1
+                }
+                if(direction === 'south') {
+                    y = y - 1
+                }
+                if(direction === 'north') {
+                    y = y + 1
+                }
+            
+            element.style.left = x + 'px'//???
+            element.style.bottom = y + 'px'
+            }
+            
+            setInterval(moveCharacter, 1)                  //passes the funtion to setInterval and moves greenie every 1 millisecond
+            
+            document.addEventListener('keydown', function (e){   //change greenie's direction with arrow keys
+            
+                if(e.repeat) return;                  //Where do "e.repeat, e.key,"come from? Not defined in program
+            
+                if(e.key === "ArrowLeft"){
+                    direction ='west'
+                }
+                if(e.key === 'ArrowUp'){
+                    direction = 'north'
+                }
+                if (e.key === "ArrowRight"){
+                    direction = 'east'
+                }
+                if (e.key === 'ArrowDown'){
+                    direction = 'south'
+                }
+                callback(direction)   //callback where we change the direction variable When we add "direction to callback we are putting callback into scope"
+            })
+            
+            document.addEventListener('keyup', function (e){    //greenie stops when key is lifted up
+                direction = null
+                callback(direction)     //callback were we change the direction variable putting callback into scope
+            })
+    }
 
     return{
-        to: moveToCoordinates
+        to: moveToCoordinates,
+        withArrowKeys: moveWithArrowKeys
     }
 }
 
