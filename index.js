2a64f0e00f86b68c63caba90e22fab623c529387
Ramona Snider <41329897+rlsnider@users.noreply.github.com>
2022-08-06 06:09:52 -0700

    restarted

--- index.js
+++ index.js
@@ -2,29 +2,6 @@ const inventory = newInventory()
 move(inventory).to(0, 0)
 
 const character = newImage('assets/green-character/static.gif')
-
-function handleDirectionChange(direction){   //wrap direction instructions for movement gifs in function for scope
-    if(direction === null){
-        character.src = 'assets/green-character/static.gif'
-    }
-    if(direction === 'west'){
-        character.src = 'assets/green-character/west.gif'
-    }
-    if(direction === 'east'){
-        character.src = 'assets/green-character/east.gif'
-    }
-    if(direction === 'north'){
-        character.src = 'assets/green-character/north.gif'
-    }
-    if(direction === 'south'){
-        character.src = 'assets/green-character/south.gif'
-    }
-}
-
-move(character).withArrowKeys(100,250, handleDirectionChange)
-let direction = null; //so we can track character with a variable 
-let x = 100;
-let y = 250;
 move(character).to(100, 250)
 
 
